34f331ef2a9dbcc56b86c4e65476602a79936026
michael <chenzixing2009@gmail.com>
2017-03-31 18:39:56 -0700

    modified:   prewowkb/Action/account.php prewowkb/Tpl/account/index.html

--- prewowkb/Action/account.php
+++ prewowkb/Action/account.php
@@ -20,9 +20,38 @@ class account extends Action{
 	
 	function ACT_index(){
 		$obj_tmp_zuopins = load("account_tmp_zuopin");
-		$res = $obj_tmp_zuopins->getList("*", array("visible"=>1), 12);
+		$res = $obj_tmp_zuopins->getAll("*", array("visible"=>1));
 		
-		$this->assign("res", $res);
+		$jiaju = array();
+		$kongjian = array();
+		$shinei = array();
+		$landscape = array();
+		$building = array();
+		
+		
+		if(!empty($res)){
+			foreach ($res as $val){
+				if($val['category'] == "furnish")
+					$jiaju[] = $val;
+				else if($val['category'] == "space")
+					$kongjian[] = $val;
+				else if($val['category'] == "construct")
+					$shinei[] = $val;
+				else if($val['category'] == "landscape")
+					$landscape[] = $val;
+				else if($val['category'] == "building")
+					$building[] = $val;
+					
+			}
+			
+		}
+		
+		$this->assign("jiaju", $jiaju);
+		$this->assign("kongjian", $kongjian);
+		$this->assign("shinei", $shinei);
+		$this->assign("landscape", $landscape);
+		$this->assign("building", $building);
+		
 	}
 	
 	function ACT_form_apply_designer(){
