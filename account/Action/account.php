057b3c9c3b58667df65f632997987548983d0536
michael <chenzixing2009@gmail.com>
2017-04-21 18:47:09 -0700

    modified:   account/Action/account.php

--- account/Action/account.php
+++ account/Action/account.php
@@ -10,9 +10,9 @@ class account extends Action{
 		$this->obj_user_favorite = load("account_user_favorite");
 		
 		$userid = isset($_SESSION['userid'])?$_SESSION['userid']:"";
-		if(empty($userid)){
-			go("/");
-		}
+// 		if(empty($userid)){
+// 			go("/");
+// 		}
 		$this->assign("userid", $userid);
 		$user_type_arr = $this->obj_user->getOne("*", array("uid"=>$userid, "visible"=>1));
 		$user_type = $user_type_arr['account_type'];
